24d104f587ba597a406f70f5c633ebd820231541
Gaz Davidson <gaz@bitplane.net>
2009-12-09 05:57:22 +0000

    Removed extra class, it now uses tuples for sorting. Added iter and delitem methods

--- InterpoList.py
+++ InterpoList.py
@@ -13,18 +13,6 @@ from bisect import bisect, bisect_left
 from math import fabs
 import string
 
-class InterpoListItem(object):
-    def __init__(self, key, value):
-        """ The constructor """
-        self.key   = key
-        self.value = value
-    def __lt__(self, other):
-        """ The less than operator, required for sorting """
-        return self.key < other.key
-    def __repr__(self):
-        """ Formal representation of the object """
-        return "%s(%s,%s)" % (type(self).__name__, self.key, self.value)
-    
 class InterpoList(object):
     """
         A list type which automatically does linear interpolation.
@@ -40,53 +28,68 @@ class InterpoList(object):
     """
     def __init__(self, data = {}):
         """ constructor """
-        self.items = list()
-        for key in data:
-            self.__setitem__(key, data[key])
+        self.items = data.items()
+        self.items.sort()
 
     def __getitem__(self, index):
         """ Returns the value for a given index """
         # create a dummy item for searching
-        idx = float(index)
-        item = InterpoListItem(idx, 0)
+        findex = float(index)
+        item   = (findex, 0)
         # find the position where it would be inserted
         i = bisect(self.items, item)
 
-        if i == 0: # or i > len(self.items):
+        if i == 0 and self.items[0][0] != findex: # or i > len(self.items):
             # refuse to extrapolate
-            raise ValueError("Extrapolation is not supported")
+            raise IndexError("Extrapolation is not supported")
         else:
-            if self.items[i-1].key == idx:
+            if self.items[i-1].index == findex:
                 # exact
                 return self.items[i-1].value
             else:
                 if i == len(self.items):
                     # refuse to extrapolate
-                    raise ValueError("Extrapolation is not supported")
+                    raise IndexError("Extrapolation is not supported")
                 else:
                     # interpolate                    
-                    factor = (self.items[i].key - idx) / (self.items[i].key - self.items[i-1].key)
-                    return self.items[i].value - (self.items[i].value - self.items[i-1].value)*factor
+                    factor = (self.items[i][0] - findex) / (self.items[i][0] - self.items[i-1][0])
+                    return self.items[i][1] - (self.items[i][1] - self.items[i-1][1])*factor
 
-    def __setitem__(self, index, value):
-        """ adds a new index or replaces a current one """
+    def __setitem__(self, key, value):
+        """ adds a new keypoint or replaces a current one """
         # create a new list item
-        findex = float(index)
-        item   = InterpoListItem(findex, value)
+        fkey = float(key)
+        item = (fkey, value)
         # find the insertion point
         i = bisect_left(self.items, item)
         
         # replace existing value?
-        if i < len(self.items) and self.items[i].key == findex:
+        if i < len(self.items) and self.items[i].index == fkey:
             self.items[i] = item
         else:
             # insert it
             self.items.insert(i, item)
 
+
+    def __delitem__(self, key):
+        """ Deletes a given keypoint """
+        fkey = float(key)
+        item = (fkey, 0)
+        i = bisect_left(self.items, item)
+
+        # convert IndexError to KeyError
+        try:
+            if self.items[i][0] == fkey:
+                del self.items[i]
+            else:
+                raise KeyError("Key not found")
+        except IndexError:
+            raise KeyError("Key not found")
+
     def __len__(self):
         """ Returns the range of the indices """
         if len(self.items) > 0:
-            return fabs(self.items[-1].key - self.items[0].key)
+            return fabs(self.items[-1].index - self.items[0].index)
         else:
             return 0.0
 
@@ -95,9 +98,11 @@ class InterpoList(object):
         # Dump the contents into a dict style string using a list comprehension expression.
         # This makes sure there's no outer "loop" as such, which avoids allocating tons of
         # (immutable) strings of increasing length.
-        lst = string.join([string.join( (str(i.key), str(i.value) ), ":") for i in self.items], ",")
+        lst = string.join([string.join( (str(i[0]), str(i[1]) ), ":") for i in self.items], ",")
         # spit the whole thing out
         return "%s(data={%s})" % (type(self).__name__, lst)
 
-
+    def __iter__(self):
+        """ Returns an iterator which can traverse the list """
+        return self.items.__iter__()
 
